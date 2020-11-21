fd652ca293bcb16ead6a04267247da0e05b9d4ce
Sarah <sarah.gh301377@gmail.com>
2020-11-20 22:02:33 -0800

    giving input and using format in print

---
+++ Python-hard_way2.py
@@ -0,0 +1,15 @@
+print("my name is: \nSarah")
+sentence = " Sarah loves to \n\tTravel \n\teat \n\thang out with her friends "
+print(sentence)
+print("what is \a\\a doing?")
+string = "hello from the other side"
+print(f"this is  string that tells you {string}")
+print("this sentence \'{}\' is a string" .format(string) )
+print("So how old are you sarah?")
+age = input()
+print("and how tall are you?")
+height = input()
+print(f"So Sarah is {age}  old and {height} tall!")
+# you can code like this too!
+age = input("How old are you sarah?")
+print(f'sarah is {age} and this is another way to find out!')
