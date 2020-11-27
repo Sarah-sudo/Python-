73d8e031463037b56e9f5f46cf044203fdbe98a6
Sarah <sarah.gh301377@gmail.com>
2020-11-27 13:02:30 -0800

    import Python file in another

---
+++ Python-hard_way(e25B).py
@@ -0,0 +1,18 @@
+#file_name.function_name(input)
+""" name Python-hard_way(e25) is unvalid because of - and () So change the name of your files! """
+import  Python_hard_way_e25
+sentence = "All good things come to those who wait."
+words = Python_hard_way_e25.break_words(sentence)
+
+sorted_words = Python_hard_way_e25.sort_words(words)
+
+Python_hard_way_e25.print_first_word(words)
+Python_hard_way_e25.print_last_word(words)
+
+Python_hard_way_e25.print_first_word(sorted_words)
+Python_hard_way_e25.print_last_word(sorted_words)
+
+sorted_words = Python_hard_way_e25.sort_sentence(sentence)
+
+Python_hard_way_e25.print_first_and_last(sentence)
+Python_hard_way_e25.print_first_and_last_sorted(sentence)
