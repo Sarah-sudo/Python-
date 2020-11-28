34854f1594faed5ba638dd34a43a4d5aa65e9aa4
Sarah <sarah.gh301377@gmail.com>
2020-11-28 12:57:05 -0800

    If

---
+++ Python-hard_way(e29).py
@@ -0,0 +1,26 @@
+people = 100
+cats = 80
+dogs = 50
+
+if people < cats:
+    print(f"The number of humans is {people} and the number of cats is {cats}!\nToo many cats! But still lovely!")
+
+if people > cats:
+    print("The world is saved but without any beautiful eyes :( !")
+
+if people > dogs:
+    print("World does not worth it to live anymore.")
+
+if people < dogs:
+    print("World is fulled with kindness.")
+
+dogs += 5
+if people >= dogs :
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+     print("Number of people equals to dogs.")
+   
